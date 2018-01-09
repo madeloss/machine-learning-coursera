3f6173ac47d3cc1fcfb8e17f2373a4096622aeb1
Mariana de los Santos <madeloss@gmail.com>
2018-01-08 22:31:38 -0300

    Cost Function, oneVsAll and predictOneVsAll

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,13 +36,12 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+temp = theta;
+temp(1) = 0;
 
+J = (1/m) * sum(- y .* log(sigmoid(X*theta)) - ((1 - y) .* log(1 - sigmoid(X*theta)))) + ((lambda/(2*m)) * sum(temp.^2));
 
-
-
-
-
-
+grad = ((1/m) .* X' * (sigmoid(X * theta) - y)) + ((lambda/m) .* temp); 
 
 
 % =============================================================
